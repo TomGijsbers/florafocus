7861fa058cc971e3ab19c398ac3a6a38d72feedb
TomGijsbers <r0878924@student.thomasmore.be>
2024-10-06 15:30:59 +0200

    sytling leaderbaord

--- lib/pages/leaderboard_page.dart
+++ lib/pages/leaderboard_page.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import '../api/api_service.dart'; // Zorg ervoor dat je dit pad hebt ingesteld
-import 'package:flutter/material.dart';
+import '../api/api_service.dart';
+import '../widgets/leaderboard_item.dart'; // Import LeaderboardItem widget
 
 class LeaderboardPage extends StatefulWidget {
   @override
@@ -21,11 +21,9 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
   Future<void> _fetchUsers() async {
     try {
       List<Map<String, dynamic>> users = await _apiService.fetchUsers();
-      // Tel het aantal gescande zaden en voeg het toe aan een lijst
       users.forEach((user) {
         user['scanned_count'] = user['scanned_seeds']?.length ?? 0;
       });
-      // Sorteer gebruikers op basis van het aantal gescande zaden, aflopend
       users.sort((a, b) => b['scanned_count'].compareTo(a['scanned_count']));
       setState(() {
         _users = users;
@@ -41,20 +39,31 @@ class _LeaderboardPageState extends State<LeaderboardPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Klassement"),
+        title: Text(
+          "Klassement",
+          style: TextStyle(fontFamily: 'Roboto'),
+        ),
+        backgroundColor: Colors.green[700],
+        centerTitle: true,
       ),
       body: _errorMessage.isNotEmpty
-          ? Center(child: Text(_errorMessage))
+          ? Center(
+              child: Text(
+                _errorMessage,
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.red,
+                  fontFamily: 'Montserrat',
+                ),
+              ),
+            )
           : ListView.builder(
               itemCount: _users.length,
               itemBuilder: (context, index) {
                 final user = _users[index];
-                return ListTile(
-                  title: Text('${user['first_name']} ${user['last_name']}'),
-                  subtitle: Text('Gescanden zaden: ${user['scanned_count']}'),
-                  leading: CircleAvatar(
-                    child: Text('${index + 1}'), // Rangnummer
-                  ),
+                return LeaderboardItem(
+                  user: user,
+                  rank: index + 1,
                 );
               },
             ),
--- lib/widgets/leaderboard_item.dart
+++ lib/widgets/leaderboard_item.dart
@@ -12,11 +12,56 @@ class LeaderboardItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text('${user['first_name']} ${user['last_name']}'),
-      subtitle: Text('Zaadjes gescand: ${user['scanned_seeds'].length}'),
-      leading: CircleAvatar(
-        child: Text(rank.toString()), // Rangnummer
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+      padding: EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.green[50],
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Row(
+        children: [
+          CircleAvatar(
+            backgroundColor: Colors.green[700],
+            child: Text(
+              rank.toString(),
+              style: TextStyle(
+                color: Colors.white,
+                fontFamily: 'Montserrat',
+              ),
+            ),
+          ),
+          SizedBox(width: 16.0),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '${user['first_name']} ${user['last_name']}',
+                style: TextStyle(
+                  fontSize: 18,
+                  color: Colors.green[900],
+                  fontFamily: 'Montserrat',
+                ),
+              ),
+              Text(
+                'Zaadjes gescand: ${user['scanned_seeds'].length}',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.green[700],
+                  fontFamily: 'Montserrat',
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
