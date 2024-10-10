5bdf2aa304f74009e9418d247f6774e44d466a97
TomGijsbers <r0878924@student.thomasmore.be>
2024-10-10 19:36:48 +0200

    commentaar main

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,65 @@
 import 'package:flutter/material.dart';
-import 'pages/login_page.dart'; // Ensure this import is correct
+
+import 'pages/login_page.dart'; // Zorg ervoor dat deze import correct is
+
 import 'pages/home_page.dart';
+
 import 'pages/profile_page.dart';
+
 import 'pages/stats_page.dart';
+
 import 'pages/leaderboard_page.dart';
 
+// Hoofdfunctie om de app te starten
+
 void main() {
-  runApp(MyApp());
+  runApp(MyApp()); // Start de MyApp klasse
 }
+
+// Hoofdapplicatieklasse
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      initialRoute: '/',
+      debugShowCheckedModeBanner: false, // Verberg de debug banner
+
+      initialRoute: '/', // Stel de initiële route in op de loginpagina
+
       routes: {
         '/': (context) =>
-            LoginPage(), // Ensure LoginPage is correctly referenced
+            LoginPage(), // Verwijs naar de LoginPage als de initiële route
+
         '/profile': (context) => ProfilePage(
-            user: ModalRoute.of(context)!.settings.arguments
-                as Map<String, dynamic>),
-        '/leaderboard': (context) => LeaderboardPage(),
+            user: ModalRoute.of(context)!.settings.arguments as Map<String,
+                dynamic>), // Haal de gebruikersdata op uit de instellingen van de route
+
+        '/leaderboard': (context) =>
+            LeaderboardPage(), // Verwijs naar de LeaderboardPage
       },
+
+      // Dynamisch routes genereren
+
       onGenerateRoute: (settings) {
         if (settings.name == '/home') {
-          final user = settings.arguments as Map<String, dynamic>;
+          final user = settings.arguments
+              as Map<String, dynamic>; // Haal gebruikersdata op
+
           return MaterialPageRoute(
-            builder: (context) => HomePage(user: user),
+            builder: (context) =>
+                HomePage(user: user), // Bouw de HomePage met gebruikersdata
           );
         } else if (settings.name == '/statistics') {
-          final user = settings.arguments as Map<String, dynamic>;
+          final user = settings.arguments
+              as Map<String, dynamic>; // Haal gebruikersdata op
+
           return MaterialPageRoute(
-            builder: (context) => StatisticsPage(user: user),
+            builder: (context) => StatisticsPage(
+                user: user), // Bouw de StatisticsPage met gebruikersdata
           );
         }
-        return null;
+
+        return null; // Retourneer null als er geen overeenkomende route is
       },
     );
   }
