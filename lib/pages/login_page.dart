c91888353ffb36f451b95377fa830489b33428f2
NicolasVanDyck <r0878921@student.thomasmore.be>
2024-10-19 11:18:27 +0200

    more api fix

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -33,7 +33,7 @@ class _LoginPageState extends State<LoginPage> {
       );
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Login mislukt. Controleer uw inloggegevens.')),
+        const SnackBar(content: Text('Login mislukt. Controleer uw inloggegevens.')),
       );
     }
   }
@@ -52,7 +52,7 @@ class _LoginPageState extends State<LoginPage> {
 
     if (!_isValidEmail(email)) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Ongeldig e-mailadres.')),
+        const SnackBar(content: Text('Ongeldig e-mailadres.')),
       );
       return;
     }
@@ -71,7 +71,7 @@ class _LoginPageState extends State<LoginPage> {
 
     if (response.statusCode == 200) {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Account succesvol aangemaakt.')),
+        const SnackBar(content: Text('Account succesvol aangemaakt.')),
       );
       try {
         Map<String, dynamic> user = jsonDecode(response.body);
@@ -89,7 +89,7 @@ class _LoginPageState extends State<LoginPage> {
       }
     } else {
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text('Account aanmaken mislukt.')),
+        const SnackBar(content: Text('Account aanmaken mislukt.')),
       );
     }
   }
@@ -99,7 +99,7 @@ class _LoginPageState extends State<LoginPage> {
       context: context,
       builder: (BuildContext context) {
         return AlertDialog(
-          title: Text('Maak een nieuw account aan'),
+          title: const Text('Maak een nieuw account aan'),
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
@@ -107,12 +107,12 @@ class _LoginPageState extends State<LoginPage> {
                 label: "Email",
                 controller: newEmailController,
               ),
-              SizedBox(height: 16),
+              const SizedBox(height: 16),
               LoginTextField(
                 label: "Naam",
                 controller: newNameController,
               ),
-              SizedBox(height: 16),
+              const SizedBox(height: 16),
               LoginTextField(
                 label: "Wachtwoord",
                 obscureText: true,
@@ -125,23 +125,23 @@ class _LoginPageState extends State<LoginPage> {
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: Text('Annuleren'),
+              child: const Text('Annuleren'),
             ),
             ElevatedButton(
               onPressed: _createAccount,
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.green[600],
-                padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
-                textStyle: TextStyle(
+                textStyle: const TextStyle(
                   fontSize: 18,
                   fontWeight: FontWeight.bold,
                   fontFamily: 'Roboto',
                 ),
               ),
-              child: Text('Aanmaken'),
+              child: const Text('Aanmaken'),
             ),
           ],
         );
@@ -153,7 +153,7 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login"),
+        title: const Text("Login"),
         backgroundColor: Colors.transparent,
         elevation: 0,
       ),
@@ -168,14 +168,14 @@ class _LoginPageState extends State<LoginPage> {
             end: Alignment.bottomRight,
           ),
         ),
-        padding: EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(16.0),
         child: Center(
           child: SingleChildScrollView(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(height: 40),
-                Text(
+                const SizedBox(height: 40),
+                const Text(
                   "Welkom bij de FloraFocus",
                   style: TextStyle(
                     fontSize: 28,
@@ -184,50 +184,50 @@ class _LoginPageState extends State<LoginPage> {
                     fontFamily: 'Montserrat',
                   ),
                 ),
-                SizedBox(height: 40),
+                const SizedBox(height: 40),
                 LoginTextField(
                   label: "Email",
                   controller: emailController,
                 ),
-                SizedBox(height: 16),
+                const SizedBox(height: 16),
                 LoginTextField(
                   label: "Wachtwoord",
                   obscureText: true,
                   controller: passwordController,
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: _login,
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.green[600],
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                    padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10),
                     ),
-                    textStyle: TextStyle(
+                    textStyle: const TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
                       fontFamily: 'Roboto',
                     ),
                   ),
-                  child: Text("Login"),
+                  child: const Text("Login"),
                 ),
-                SizedBox(height: 20),
+                const SizedBox(height: 20),
                 ElevatedButton(
                   onPressed: _showCreateAccountDialog,
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.green[600],
-                    padding: EdgeInsets.symmetric(horizontal: 40, vertical: 15),
+                    padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(10),
                     ),
-                    textStyle: TextStyle(
+                    textStyle: const TextStyle(
                       fontSize: 18,
                       fontWeight: FontWeight.bold,
                       fontFamily: 'Roboto',
                     ),
                   ),
-                  child: Text("Maak een nieuw account aan"),
+                  child: const Text("Maak een nieuw account aan"),
                 ),
               ],
             ),
